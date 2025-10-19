d4c7ea5969284944c57d08f832401f45edcb33f3
Sarim Ahmed <C41f0N@protonmail.com>
2025-10-19 17:28:31 +0500

    fix: update label text for code input field

--- lib/screens/code_entry_page.dart
+++ lib/screens/code_entry_page.dart
@@ -109,7 +109,7 @@ class _CodeEntryPageState extends State<CodeEntryPage> {
               child: TextField(
                 controller: _ctrl,
                 decoration: InputDecoration(
-                  labelText: 'Code',
+                  labelText: 'Code RAAAAAAHIIIIIIMMMMM',
                   errorText: _error,
                 ),
                 onSubmitted: (_) => _submit(),
